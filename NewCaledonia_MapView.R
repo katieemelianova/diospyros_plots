b212e2f15de1a0e755bdd2c6a9bd8c08be4689fa
Katie Emelianova <katieemelianova@eduroam-021-174.wlan.univie.ac.at>
2022-10-14 18:15:49 +0200

    muck about with colours

--- NewCaledonia_MapView.R
+++ NewCaledonia_MapView.R
@@ -2,6 +2,8 @@ library(sf)
 library(mapview)
 library(readr)
 library(dplyr)
+library(magrittr)
+library(RColorBrewer)
 # this post is very useful for understanding sf objects
 # https://www.jessesadler.com/post/simple-feature-objects/#:~:text=The%20most%20likely%20way%20to,an%20sf%20object%20from%20scratch.
 
@@ -20,14 +22,17 @@ mapview(all)
 mapview(all, zcol = c("weather", "language"))
 
 
-diospyros_localities<-read_tsv("/Users/katieemelianova/Desktop/Diospyros/Diospyros_Localities.tsv")
+diospyros_localities<-read_tsv("/Users/katieemelianova/Desktop/Diospyros/diospyros_plots/Diospyros_Localities.tsv")
+diospyros_localities %<>% filter(Latitude != 0 & !(is.na(Soil)))
+
 
 test<-st_as_sf(x = diospyros_localities, coords = c("Longitude", "Latitude"), crs=4642)
 
+cols<-brewer.pal(7, "Set1")
+# some nice coloirs that got picked which happened to look nice
+#cols<-c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3", "#FF7F00", "#FFFF33", "#A65628")
 
-
-
-mapview(test, zcol="Soil")
+mapview(test, zcol="Soil", col.regions = cols)
 
 
 
