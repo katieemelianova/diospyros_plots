da953c674a0e0b2bd6b88f9a87be77a9ebd33bfc
Katie Emelianova <katieemelianova@Katies-MacBook-Air.local>
2023-08-23 13:32:20 +0200

    debug and refactor so the tree works

--- NewCaledonia_MapView.R
+++ NewCaledonia_MapView.R
@@ -6,8 +6,27 @@ library(magrittr)
 library(RColorBrewer)
 library(tidyr)
 library(ggtree)
-library(ape)
+#library(ape)
 library(stringr)
+library(reshape2)
+library(ggnewscale)
+library(ggtreeExtra)
+library(ggplot2)
+library(cowplot)
+library(gridExtra)
+library(grid)
+library(ggpubr)
+
+# I had some issues installing terra, one of the things was that the timeout limit was being reached
+# When I extended this time limit it completed fine
+#options(timeout=100)
+#install.packages("terra")
+
+
+packageurl <- "https://cran.r-project.org/src/contrib/Archive/terra/terra_1.7-3.tar.gz"
+
+install.packages(packageurl, repos=NULL, type="source")
+
 # this post is very useful for understanding sf objects
 # https://www.jessesadler.com/post/simple-feature-objects/#:~:text=The%20most%20likely%20way%20to,an%20sf%20object%20from%20scratch.
 
@@ -32,7 +51,13 @@ mapview(all, zcol = c("weather", "language"))
 
 # read in a modified version of the online spreadsheet; I removed some columns and renamed some of the soil types to match
 # e.g. Seprpentines and Serpetine both become Serpentine
-diospyros_localities<-read_tsv("/Users/katieemelianova/Desktop/Diospyros/diospyros_plots/Diospyros_Localities.tsv")
+# update 22.08.23: we realised that a lot of the soil labels were incorrect and were taken from field notes. Correct mappings of soil to sample 
+# are in the Paun et al. 2016 supplementary figure 5, though in a coloir coded tree form
+# I took the designations from supp fig 5 and re-annotated the soil column based on this
+# where there is no population data, soiltype is simply transposed. Where there is population data but all individuals in the supp figure 
+# are the same, I transpose that soil type of all samples in the table. Where there are a mix of soiltypes per species
+# and not all individuals are available in the tree for the table, not-included individuals in the table are left blank for soiltype
+diospyros_localities<-read_tsv("/Users/katieemelianova/Desktop/Diospyros/diospyros_plots/Diospyros_Localities_corrected2016.tsv")
 
 # Remove fields which dont have a location and dont have soiltype info
 diospyros_localities %<>% filter(Latitude != 0 & !(is.na(Soil)))
@@ -59,37 +84,83 @@ mapview(diospyros_sfc, zcol="Soil", col.regions = cols)
 # read in original tree
 nwk<-ape::read.tree("/Users/katieemelianova/Desktop/Diospyros/diospyros_plots/RadiatingSpeciesDiospyros_ingrp-inds.nwk")
 
-# plot the tree in cirlular
-ggtree(nwk, layout="circular")
-
 # split the tip name column by BT to get the population names
 # I dont understand the format of this function and I dont like it at all
 # but it works so whatevs
-species<-sapply(strsplit(nwk$tip.label,"BT"), `[`, 1)  
+# also do some data cleaning
+species<-sapply(strsplit(nwk$tip.label,"BT"), `[`, 1)
+nwk$tip.label[nwk$tip.label =="impolitaBt103"] = "impolitaBT103"
+sample_id<-sapply(strsplit(nwk$tip.label,"BT"), `[`, 2)
+sample_id<-paste("BT", sample_id, sep = "")
+sample_id[sample_id == "BTNA"] = nwk$tip.label[sample_id =="BTNA"]
+sample_id<-sapply(strsplit(sample_id, "_"), `[`, 1)
+sample_id<-sapply(strsplit(sample_id, "-"), `[`, 1)
 
 # set the species in the newick object
 nwk$species <- species
+nwk$sample_id<-sample_id
+
+
+
+# take the original data frame from the map part and take the rad and BT sample name columns only
+mapping_loc<-diospyros_localities %>% 
+  dplyr::select(`RAD localities`, `sequenced samples`, Soil) %>% 
+  data.frame()
+
+# this is a really dumb way to do it but whatever it works
+# apply over each row, split the BTXXX column by ", ", and then add it to a data frame, where the other column is the RAD number
+# this gives you a list of data frames, which are bound into one using bind_rows
+bt_soil_mapping<-apply(mapping_loc, 1, function(x) strsplit(x[2], ", ") %>% data.frame(rad=x[3])) %>% bind_rows()
+
+# remove the weird rownames the apply function gives it
+rownames(bt_soil_mapping) <-NULL
+colnames(bt_soil_mapping)<-c("sample_id", "soil")
+
+# remove duplicate samples
+bt_soil_mapping_noduplicates <- bt_soil_mapping %>% filter(sample_id != "BT147" & sample_id != "BT296")
+
+# join tip names and soil types
+bt_soil_mapping_joined<-left_join(data.frame(sample_id=nwk$sample_id), bt_soil_mapping_noduplicates) %>% dplyr::select(soil)
+
+#set rownames to corresponding tip labels
+rownames(bt_soil_mapping_joined) <- nwk$tip.label
+
 
 # make a list where item name is species name and objects within are the tip labels belonging to that species
 groupInfo<-split(nwk$tip.label, nwk$species)
 
 # use groupOTU to group the tips by species and plot
 nwk_grouped<-groupOTU(nwk, groupInfo, group_name = "species")
-ggtree(nwk_grouped, aes(color=species), layout='circular') + geom_tiplab(size=3.2) + theme(legend.position = "None")
-dev.off()
 
 # take the same tree as above but make the tip labels the species name only (no population info)
 nwk_grouped_speciesonly<-nwk_grouped
 nwk_grouped_speciesonly$tip.label <- nwk_grouped_speciesonly$species
-ggtree(nwk_grouped_speciesonly, aes(color=species), layout='circular') + geom_tiplab(size=4) + theme(legend.position = "None")
+
+# first make one tree which is grouped and coloured by species
+circ<-ggtree(nwk_grouped, aes(color=species), layout='circular') + geom_tiplab(size=5) 
+
+circ_soil<-gheatmap(circ, bt_soil_mapping_joined, offset=.012, width=.05, colnames=FALSE) + 
+  scale_fill_viridis_d(option = "C", name = "Clade", na.value = "gray94") + theme(legend.position="none")
+
+# make the legend for the soiltypes
+circ_nolegend<-ggtree(nwk)
+soiltype_values<-bt_soil_mapping_joined %>% dplyr::select(soil)
 
 
+# repeat this for soiltype
+soiltype_legend<-gheatmap(circ_nolegend, soiltype_values, offset = .01, width = 0.04,
+                          colnames = FALSE,
+                          colnames_offset_y = 1) +
+  scale_fill_viridis_d(option = "C", na.value = "gray94", name = "soiltype"
+                       #scale_fill_viridis_d(option = "C", na.value = "gray94", name = "Soiltype", labels = c("Ironcrust", "Kalcarious", "Serpentine", "Ultramafic", "Volcano-Sedimentary", "Unknown")
+                       #scale_fill_manual(values=mycolors, name="Soiltype"
+  ) + theme(legend.text=element_text(size=25), legend.title= element_blank())
 
-# this is a  handy bit of code to make up a random tree and plot it in different ways
-#tr <- rtree(10)
-#tr$tip.label = gsub('^', 'A long label: ', tr$tip.label)
-#ggtree(tr, branch.length = 'none') + geom_tiplab()
+# get the soiltype legend
+soiltype_legend <- get_legend(soiltype_legend)
 
-
+#pdf("tree_colour3.pdf", width=25, height=25)
+ggarrange(circ_soil, soiltype_legend, widths = c(8,2))
+#dev.off()
 
 
